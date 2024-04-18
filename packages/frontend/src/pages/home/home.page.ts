93c612bf8917218750d46a28b6c9d11b307330af
William Lee <me@william-lee.com>
2024-04-18 22:28:33 +1000

    feat: add filter

--- packages/frontend/src/pages/home/home.page.ts
+++ packages/frontend/src/pages/home/home.page.ts
@@ -3,20 +3,36 @@ import { CheeseServiceModule } from '../../features/cheese-service/cheese-servic
 import { CheeseListComponent } from '../../features/cheese-list/cheese-list.component';
 import { Cheese } from '@cheesus/contracts';
 import { CheeseService } from '../../features/cheese-service/cheese.service';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'home',
   templateUrl: './home.page.html',
-  imports: [CheeseServiceModule, CheeseListComponent]
+  // align right
+  styles: [`.home-menu { text-align: center; }`],
+  imports: [CheeseServiceModule, CheeseListComponent, TitleCasePipe]
 })
 export class HomePage implements OnInit {
-  cheeses: Cheese[] = []
+  allCheeses: Cheese[] = []
+  displayedCheeses: Cheese[] = []
+  colors: string[] = []
+  selectedColor: string | undefined
 
   constructor(private readonly cheeseService: CheeseService) {}
 
   async ngOnInit(): Promise<void> {
-    this.cheeses = await this.cheeseService.getCheeses()
-    console.log(this.cheeses)
+    const fetchedCheeses = await this.cheeseService.getCheeses()
+    this.displayedCheeses = this.allCheeses = fetchedCheeses
+    this.colors = [...new Set(this.allCheeses.map(cheese => cheese.color))]
+  }
+
+  onColorFilterClick(color: string | undefined): void {
+    this.selectedColor = color
+    if (color) {
+      this.displayedCheeses = this.allCheeses.filter(cheese => cheese.color === color)
+    } else {
+      this.displayedCheeses = this.allCheeses
+    }
   }
 }
